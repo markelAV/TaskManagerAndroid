f05061689eb085e4153dcbcbb003f83d8087303d
artem <markelov19999180@mail.ru>
2020-12-17 23:59:32 +0400

    Add work with GPS

--- app/src/main/java/com/example/taskmanagerandroid/CreateEditTask.java
+++ app/src/main/java/com/example/taskmanagerandroid/CreateEditTask.java
@@ -2,7 +2,11 @@ package com.example.taskmanagerandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -12,18 +16,40 @@ import com.example.taskmanagerandroid.data.model.Task;
 
 public class CreateEditTask extends AppCompatActivity {
 
-    EditText viewName;
-    EditText viewDate;
-    EditText viewTime;
-    EditText viewDescription;
+    private EditText viewName;
+    private EditText viewDate;
+    private EditText viewTime;
+    private EditText viewDescription;
     private String taskId = null;
+    private Location currentLocation;
 
-    DatabaseAdapter adapter;
+    private final long LOCATION_REFRESH_TIME = 5000;
+    private final float LOCATION_REFRESH_DISTANCE = 10;
+
+    private DatabaseAdapter adapter;
+    private LocationManager locationManager;
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(final Location location) {
+            currentLocation = location;
+            System.out.println("[DEBUG] my location is " + location.toString()) ;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_edit_task);
+
+        currentLocation = null;
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        try {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME,
+                    LOCATION_REFRESH_DISTANCE, mLocationListener);
+        }
+        catch (SecurityException ex) {
+            System.out.println("[ERROR] security " + ex.getMessage());
+        }
 
         this.viewName = (EditText) findViewById(R.id.edit_text_task_name);
         this.viewDate = (EditText) findViewById(R.id.edit_text_date);
@@ -72,4 +98,31 @@ public class CreateEditTask extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+    public void addLocation(View view) {
+        StringBuilder stringBuilder = new StringBuilder(viewDescription.getText().toString());
+        String currentLocation = getCurrentLocation();
+        if(currentLocation != null) {
+            stringBuilder.append("GPS: ").append(currentLocation);
+        }
+
+        viewDescription.setText(stringBuilder.toString());
+    }
+
+    //Fixme please use StringBuilder
+    private String getCurrentLocation() {
+        String location = null;
+        if(this.currentLocation != null) {
+            String[] locations = currentLocation.toString().split(" ");
+            if(locations != null && locations.length > 3) {
+                String[] cords = locations[1].split(",");
+                if(cords != null && cords.length > 3) {
+                    location = "[" + cords[0] + "." + cords[1] +", " +cords[2] + "." + cords[3] + "]"; // ГОСТ 5812-82
+                }
+            }
+            String cord = currentLocation.toString();
+        }
+        return  location;
+    }
+
 }
